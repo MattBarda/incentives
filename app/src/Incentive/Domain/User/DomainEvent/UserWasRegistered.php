bad9d859faf9150d8c13eaadc8035a3f7fcaa333
mattbarda <mateuszbarda@gmail.com>
2022-08-25 20:19:41 +0200

    Register User functionality.

---
+++ app/src/Incentive/Domain/User/DomainEvent/UserWasRegistered.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Incentive\Domain\User\DomainEvent;
+
+use App\Incentive\Domain\User\ValueObject\EmailAddress;
+use App\Incentive\Domain\User\ValueObject\UserId;
+use App\Incentive\Domain\User\ValueObject\UserName;
+use Broadway\Serializer\Serializable;
+
+
+class UserWasRegistered implements Serializable
+{
+    private UserId $userId;
+    private UserName $userName;
+    private EmailAddress $emailAddress;
+
+    public function __construct(UserId $userId, UserName $userName, EmailAddress $emailAddress)
+    {
+        $this->userId = $userId;
+        $this->userName = $userName;
+        $this->emailAddress = $emailAddress;
+    }
+
+    public function userId(): UserId
+    {
+        return $this->userId;
+    }
+
+    public function userName(): UserName
+    {
+        return $this->userName;
+    }
+
+    public function emailAddress(): EmailAddress
+    {
+        return $this->emailAddress;
+    }
+
+    public static function deserialize(array $data): self
+    {
+        return new self(
+            UserId::fromString($data['basketId']),
+            UserName::fromString($data['userName']),
+            EmailAddress::fromString($data['emailAddress'])
+        );
+    }
+
+    public function serialize(): array
+    {
+        return [
+            'userId' => $this->userId->toString(),
+            'userName' => $this->userName->toString(),
+            'emailAddress' => $this->emailAddress->toString()
+        ];
+    }
+}
